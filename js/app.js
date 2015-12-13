93bdc8c879ec207ea249437b385a41b914ef5de1
Nick-Nordale <nicknordale@gmail.com>
2015-12-12 17:10:16 -0800

    Blah blah

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function() {
+    //Montego Bay, Jamaica
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=18.47&lon=-77.92&APPID=6584681eea3d012963220225316e90ec").then(function(data) {
         var weather = data.main.temp;
         var image = data.weather[0].icon;
