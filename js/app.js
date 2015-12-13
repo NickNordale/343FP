e7a30b9f62bd1cd7510203611425113cd7f5b83a
Nick-Nordale <nicknordale@gmail.com>
2015-12-12 17:36:20 -0800

    Started map stuff

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 
 $(document).ready(function() {
+    'use strict';
+
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'nicknordale.nnj8a31p',
+        accessToken: 'pk.eyJ1Ijoibmlja25vcmRhbGUiLCJhIjoiY2lmdTN2b2JnMWs3aHVna3I4bGxqMDNzNSJ9.yNkphJiLTNJMsaoOqJYrJQ'
+    }).addTo(map);
+
+    var circle = L.circle([51.508, -0.11], 200, {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5
+    }).addTo(map);
+
     //Montego Bay, Jamaica
     $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=18.47&lon=-77.92&APPID=6584681eea3d012963220225316e90ec").then(function(data) {
         var weather = data.main.temp;
